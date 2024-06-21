a9b98a27ae8b707569427acdd4709bd3dd58bb72
sudipstha08 <sudipstha08@gmail.com>
2024-06-21 13:27:52 +0545

    feat: add header

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
 
-const Header: FC = () => {
+export const Header: FC = () => {
   return (
-    <header className="px-4 py-6 mb-0 bg-no-repeat dark:bg-[#0B1120]">
+    <header className="px-4 py-6 mb-0 bg-no-repeat">
       <div className="container flex justify-between max-w-1200 m-auto">
         <img src="/assets/imgs/dnd3.png" width="200" alt="logo" />
         <h1 className="text-5xl text-slate-400 font-concert">
@@ -12,5 +12,3 @@ const Header: FC = () => {
     </header>
   )
 }
-
-export { Header }
