34dd63c7bca25fe69278514465db064d26457ace
sudipstha08 <sudipstha08@gmail.com>
2024-08-25 22:36:26 +0545

    feat: add dayjs

--- src/containers/SpellsPage/index.tsx
+++ src/containers/SpellsPage/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react'
 import { Header, Spells, Window } from '@/components'
+import * as dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
 
 export const SpellsPage: FC = () => {
+  // eslint-disable-next-line no-console
+  console.log(dayjs.utc().format())
   return (
     <main className="dark:bg-[#0B1120] h-lvh">
       <Header />
